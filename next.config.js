a56995add6e1042dc93a2fcab386df1cb7de40cb
rdemora2 <robertomoraeszar@gmail.com>
2026-02-24 00:11:43 -0300

    BUILD/ atualiza next do 14 para 16 e cria sw,js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
---
+++ public/sw.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-restricted-globals */
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
